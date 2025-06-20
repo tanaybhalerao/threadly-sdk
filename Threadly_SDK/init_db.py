af0c06763e2e2f77ef177d15ab0700ee84c8940e
Tanay Bhalerao <tanay@Tanays-MacBook-Air.local>
2025-06-19 17:54:58 -0700

    Cleaned up repo to keep SDK only

--- Threadly_SDK/init_db.py
+++
@@ -1,7 +0,0 @@
-from .db_setup import engine
-from .models import Base
-
-# Create all tables defined with Base (just MemoryEvent for now)
-Base.metadata.create_all(bind=engine)
-
-print("Database and tables created.")
--- clear_vector_memory.py
+++
@@ -1,5 +0,0 @@
-# clear_vector_memory.py
-from embedding_utils import init_faiss, print_vector_count
-
-init_faiss()
-print_vector_count()
